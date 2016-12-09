13926ac2fb016c1ac73d92c30a2984d8dd0c0c91
Tuomo Hartonen <tuomo.hartonen@helsinki.fi>
2016-12-09 09:59:10 +0200

    Corrected a minor bug that caused the FDR estimates of peaks with identical p-values but different rank to differ slightly from each other.

--- src/FDR.py
+++ src/FDR.py
@@ -11,10 +11,9 @@
 # along with this program. If not, see http://www.gnu.org/licenses/.
 import argparse
 import csv
-
 import numpy as np
-
 from operator import itemgetter
+
 
 def FDR():
 
@@ -31,7 +30,7 @@ def FDR():
 
     #OPTIONAL PARAMETERS
     #if these are not given, defaults are used
-    parser.add_argument("-t","--input_type",help="1=peakC (default), 2=MACE",type=int,choices=[1,2],default=1)
+    parser.add_argument("-t","--test_type",help="BH=Benjamini-Hochberg (default), BY=Benjamini-Hochberg-Yekutieli.",type=str,choices=['BH','BY'],default='BH')
     parser.add_argument("-m","--num_tests",help="Total number of tests conducted. If not given, the input file is assumed to contain all the test results.",type=int,default=None)
     args = parser.parse_args()
 
@@ -47,29 +46,29 @@ def FDR():
             data.append(row)
 
     #sorting the list according to p-value (secondary by score)
-    if args.input_type==2: data = sorted(data,key=lambda x: x[-1])
-    else: data = sorted(data,key=lambda x: (x[5],-x[6]))
+    data = sorted(data,key=lambda x: (x[5],-x[6]))
 
     if args.num_tests==None: m = len(data)
     else: m = args.num_tests
 
     #calculating the Benjamini-Hochberg corrected p-values
     #https://en.wikipedia.org/wiki/False_discovery_rate#Benjamini.E2.80.93Hochberg.E2.80.93Yekutieli_procedure
-    c = sum([1/i for i in range(1,m)])
-    for k in range(1,len(data)+1):
-        P_k = data[k-1][-2]
-        alpha = P_k*m*c/k
-        data[k-1].append(alpha)
-
-
+    if args.test_type=='BY': c = sum([1.0/i for i in range(1,m)])
+    else: c = 1.0
+    inds = np.ones(len(data))
+    for i in range(0,len(data)): inds[i] = (i+1)*inds[i]
+    ps = c*m*np.array([d[-2] for d in data])/inds
+    ps = np.minimum.accumulate(ps[::-1])
+    ps = np.minimum(ps,np.ones(len(data)))
+    ps_rev = ps[::-1]
+    for i in range(len(ps_rev)): data[i].append(ps_rev[i])
+    
     #saving the results as an igv-file sorted by chromosome and location
     data = sorted(data,key=lambda x: (x[0],x[1]))
 
     with open(args.outfile,'wb') as csvfile:
         w = csv.writer(csvfile,delimiter="\t")
-        
-        if args.input_type==1: w.writerow(["chromosome","start","end","id","signal","p-value","score","FDR"])
-        else: w.writerow(["chromosome","start","end","name","p-value","FDR"])
+        w.writerow(["chromosome","start","end","id","signal","p-value","score","FDR"])
         for row in data: w.writerow(row)
 #FDR ends
 
